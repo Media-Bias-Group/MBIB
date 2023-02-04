a03a8d86add240d08505fa006e9b33a8d290a1dc
Tomas <horychtomas98@gmail.com>
2023-02-04 18:07:04 +0100

    added PHEME

--- datasets/012_PHEME/process.py
+++ datasets/012_PHEME/process.py
@@ -1,32 +1,32 @@
 import os
 import pandas as pd
 from datasets.data_utils import prepare_text
+from datasets.data_utils import TweetLoader
+import json
 
 project_path = os.getcwd()
 local_path = 'datasets/012_PHEME'
-raw_path = os.path.join(project_path,local_path,'raw')
-clean_path = os.path.join(project_path,local_path,'072-liar_dataset.csv')
+raw_path = os.path.join(project_path,local_path,'raw/all-rnr-annotated-threads')
+clean_path = os.path.join(project_path,local_path,'012-Pheme.csv')
 
 df = pd.DataFrame(columns= ['tweetID', 'text', 'label', 'rumour'])
 tweet = TweetLoader()
-topics = [ele for ele in os.listdir(ds_raw_path) if '.' not in ele]
-
-    # Non-rumours
+topics = [ele for ele in os.listdir(raw_path) if '.' not in ele]
 
 for topic in topics:
-    articles_nr = [ele for ele in os.listdir(os.path.join(ds_raw_path, topic, 'non-rumours')) if '.' not in ele] # List of all non-roumor articles
+    articles_nr = [ele for ele in os.listdir(os.path.join(raw_path, topic, 'non-rumours')) if '.' not in ele] # List of all non-roumor articles
     tweets_non_rumour = tweet.fetch_list(articles_nr) # pandas Dataframe, used Tomasz's class
     tweets_non_rumour['label'] = 0 # since there are no veracity labels for non-rumours, assumed to be true
     tweets_non_rumour['rumour'] = 0
     df = pd.concat([df, tweets_non_rumour])
 
     # rumours
-    articles_r = [ele for ele in os.listdir(os.path.join(ds_raw_path, topic, 'rumours')) if '.' not in ele]
+    articles_r = [ele for ele in os.listdir(os.path.join(raw_path, topic, 'rumours')) if '.' not in ele]
     tweets_rumour = tweet.fetch_list(articles_r)
     # to get the labels the annotation.json need to be parsed:
     label = []
     for article in tweets_rumour.loc[:]['tweetID']:
-        with open(os.path.join(ds_raw_path, topic, 'rumours', str(article), 'annotation.json'), 'r') as f:
+        with open(os.path.join(raw_path, topic, 'rumours', str(article), 'annotation.json'), 'r') as f:
             annotation = json.load(f)
         try:
             veracity = annotation['true']
@@ -42,5 +42,5 @@ for topic in topics:
     tweets_rumour['label'] = label
     tweets_rumour['rumour'] = 1
     df = pd.concat([df, tweets_rumour])
-df['text'] = df['text'].apply(prep.prepare_text)
-df.to_csv(os.path.join(wdr_path + "/Preprocessed_Datasets/012-Pheme.csv"))
+df['text'] = df['text'].apply(prepare_text)
+df.to_csv(clean_path)
--- datasets/012_PHEME/raw/README.md
+++ datasets/012_PHEME/raw/README.md
@@ -1,1 +1,10 @@
 1. Download data from [here](https://figshare.com/articles/dataset/PHEME_dataset_for_Rumour_Detection_and_Veracity_Classification/6392078) and extract into this folder.
+2. In order to process the data, you need to have an developer API access to Twitter. You can get the access for free. After you are granted the access, put the values of:
+    - API_KEY
+    - API_KEY_SECRET
+    - BEARER_TOKEN
+    - TOKEN
+    - TOKEN_SECRET
+  
+    Into this project config.py file.
+    The class TweetLoader that uses these information in order to access tweet texts can be found in `/datasets/data_utils.py`
